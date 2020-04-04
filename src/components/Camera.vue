c02b4307805a506ebfa81d65d637287ec3a68e05
leojojo <setsunal.fujita@gmail.com>
2020-04-05 03:39:08 +0900

    component

---
+++ src/components/Camera.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hoge">
+    <h2>{{ msg }}</h2>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Camera",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.hoge {
+  h2 {
+    color: lighten($primary, 10%);
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Camera msg="hogehoge" />
   </div>
 </template>
+
+<script>
+import Camera from "@/components/Camera.vue";
+
+export default {
+  name: "About",
+  components: {
+    Camera
+  }
+};
+</script>
