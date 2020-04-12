cd69d1f069c418f8b9c25402d991fca7d15ab6f3
leojojo <setsunal.fujita@gmail.com>
2020-04-13 04:11:34 +0900

    amplify + cognito

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,54 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+//import Login from "../views/Login.vue";
+import store from "../store/index.js";
+
+// Amplify
+//import Amplify, * as AmplifyModules from "aws-amplify";
+import * as AmplifyModules from "aws-amplify";
+//import { components, AmplifyEventBus } from "aws-amplify-vue";
+import { AmplifyEventBus } from "aws-amplify-vue";
+import { AmplifyPlugin } from "aws-amplify-vue";
 
 Vue.use(VueRouter);
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+function getUser() {
+  return Vue.prototype.$Amplify.Auth.currentAuthenticatedUser()
+    .then(data => {
+      if (data && data.signInUserSession) {
+        store.commit("setUser", data);
+        return data;
+      }
+    })
+    .catch(() => {
+      store.commit("setUser", null);
+      return null;
+    });
+}
+
+let user;
 
 const routes = [
   {
     path: "/",
     name: "Home",
+    meta: { requiresAuth: true },
     component: Home
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    meta: { requiresAuth: true },
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
   }
 ];
 
@@ -25,6 +56,39 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+// user management
+getUser().then(user => {
+  if (user) {
+    router.push({ path: "/" });
+  }
+});
+
+// login state
+AmplifyEventBus.$on("authState", async state => {
+  if (state === "signedOut") {
+    user = null;
+    store.commit("setUser", null);
+    router.push({ path: "/login" });
+  } else if (state === "signedIn") {
+    user = await getUser();
+    router.push({ path: "/" });
+  }
+});
+
+// redirect
+router.beforeResolve(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    user = await getUser();
+    if (!user) {
+      return next({
+        path: "/login"
+      });
+    }
+    return next();
+  }
+  return next();
 });
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>ログイン済み</h1>
+    <amplify-sign-out></amplify-sign-out>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
